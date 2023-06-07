13ad43172ee23b1ae9388d3a6f7c4a0e5e51a77b
Elise-oyi <maryeliseangbo@gmail.com>
2023-06-07 11:36:46 +0100

    made changes to wager.js file

--- src/pages/Wager.jsx
+++ src/pages/Wager.jsx
@@ -6,69 +6,6 @@ import axios from 'axios'
 
 
 
-const proxyURL = 'https://cors-anywhere.herokuapp.com/';
-const baseURL = 'https://fantasy.premierleague.com/api/';
-
-export const reqType = {
-  bootstrap : 'bootstrap-static/', //Overview
-  element : 'element-summary/', //Players (playderID)
-  events : 'events', // Get all gameweeks
-  event : 'event',  //A selected gameweek
-  entry : 'entry', //Get a team
-  elementTypes: 'element-types', // Get all player positions
-  gameweekFixtures: 'fixtures/?event', //Get all fixtures for a specified gameweek (gameweek number)
-  teams: 'teams/', //Get all teams,
-  leagueClassicStanding: 'leagues-classic/' //Get league standing at current gameweek.
-}
-
-// export const doCORSRequest = async (url) => {
-//   const response = await fetch(proxyURL + baseURL + url);
-//   const myJson = await response.json();
-//   return myJson
-// }
-
-
-const Wager = () => {
-
-  const fplUrl = `https://cors-anywhere.herokuapp.com/https://fantasy.premierleague.com/api/entry/1101/`;
-
-
-
-  axios.get(fplUrl)
-   .then(res=>console.log(res.data))
-   .catch(error=>console.log(error))
-  
-
-
-  // axios.get(fplUrl)
-  // .then((resp=>{
-  //   console.log(resp.data)
-  // }))
-  // .catch(error=>{
-  //   console.log(error)
-  // })
-  // const url = 'https://fantasy-premier-league3.p.rapidapi.com/data/types',
-
- const headers = {
-    "content-length": "812",
-    "content-type": "application/json; charset=utf-8",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-  }
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://fantasy-premier-league3.p.rapidapi.com/data/types',
-//   headers: {
-//     'X-RapidAPI-Key': 'd6533fae6amshd30484bbe4ef096p19ffa0jsn65580ea13803',
-//     'X-RapidAPI-Host': 'fantasy-premier-league3.p.rapidapi.com'
-//   }
-// };
-
-
-
-
-
 
 
 
